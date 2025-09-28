3273eb290975794e73b273d2c30a6351c882d248
yyi98 <dudrn170424@gmail.com>
2025-09-28 16:34:00 +0900

    Create  solution.js

---
+++ Programmers/LV 0/배열 뒤집기/solution.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120821
+// reverse() 함수 - 원본 배열 유지하기
+// reverse() 함수를 사용하면 원본 배열이 변형됩니다.
+// 원본 배열은 그대로 유지하고, 리턴되는 값만 변경하고 싶을 때는
+// 원본 배열을 복사해서 사용해야 합니다. [...arr] 구문은, 원본 배열인 arr를 복사한 새로운 배열
+// 출처 : https://hianna.tistory.com/448
+
+function solution(num_list) {
+    return num_list.reverse();
+}
