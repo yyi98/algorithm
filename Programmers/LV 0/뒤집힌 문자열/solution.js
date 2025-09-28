5063b12fbacb99f9fe7a4d784720219715bc7e20
yyi98 <dudrn170424@gmail.com>
2025-09-28 16:40:40 +0900

    Update solution.js

--- Programmers/LV 0/뒤집힌 문자열/solution.js
+++ Programmers/LV 0/뒤집힌 문자열/solution.js
@@ -1,4 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120822
+
+// 스프레드 문법 : https://jerryjerryjerry.tistory.com/154
 // join() : 배열의 모든 요소를 문자열로 결합합니다.
 function solution(my_string) {
     return [...my_string].reverse().join("");
