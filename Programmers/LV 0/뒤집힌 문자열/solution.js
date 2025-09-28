39f155b337a48252d69ec6d52aa6ba21559a9350
yyi98 <dudrn170424@gmail.com>
2025-09-28 16:39:41 +0900

    Create solution.js

---
+++ Programmers/LV 0/뒤집힌 문자열/solution.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120822
+// join() : 배열의 모든 요소를 문자열로 결합합니다.
+function solution(my_string) {
+    return [...my_string].reverse().join("");
+}
