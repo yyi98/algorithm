e9dc0b81fb1b3fd1b608f94003f4ec31cdc2883f
yyi98 <dudrn170424@gmail.com>
2025-09-28 18:49:57 +0900

    slice, splice

---
+++ Programmers/LV 0/배열 자르기/solution.js
@@ -0,0 +1,9 @@
+// slice, splice 참고
+// https://jerryjerryjerry.tistory.com/158
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120833
+function solution(numbers, num1, num2) {
+    return numbers.slice(num1, num2 + 1);
+}
+
+
