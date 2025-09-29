00bce2b59c73ebf632e2684466b0bc730a664440
yyi98 <dudrn170424@gmail.com>
2025-09-29 22:52:50 +0900

    Create  solution.js

---
+++ Programmers/LV 0/첫 번째로 나오는 음수/solution.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181896
+// https://bbaktaeho-95.tistory.com/40
+function solution(num_list) {
+    return num_list.findIndex(item => item < 0);
+}
