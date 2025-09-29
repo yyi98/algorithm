9a58d88fafb50cddbf074e2804a3cf473a5926fd
yyi98 <dudrn170424@gmail.com>
2025-09-30 00:31:25 +0900

    Update solution.js

--- Programmers/LV 0/홀짝에 따라 다른 값 반환하기/solution.js
+++ Programmers/LV 0/홀짝에 따라 다른 값 반환하기/solution.js
@@ -1,6 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181935
 function solution(n) {
-    var answer = 0;
     let isEven = n % 2 == 0;
     let arr = Array(n).fill().map((item, idx) => { return idx + 1 });
     let arr1 = arr.filter(x => isEven ? x % 2 == 0 : x % 2 !== 0);
