7cdea488b0050465be9caa015980b6b5bc345eec
yyi98 <dudrn170424@gmail.com>
2025-09-30 23:59:47 +0900

    Update solution.js

--- Programmers/LV 0/원소들의 곱과 합/solution.js
+++ Programmers/LV 0/원소들의 곱과 합/solution.js
@@ -4,3 +4,14 @@ function solution(num_list) {
     let sum_multiply_result = num_list.reduce((acc, cur) => { return acc + cur });
     return multiply_result < Math.pow(sum_multiply_result, 2) ? 1 : 0;
 }
+
+/*
+function solution(num_list) {
+    let accMul = 1, accSum = 0;
+    for (let i=0; i<num_list.length; i++) {
+        accMul *= num_list[i];
+        accSum += num_list[i];
+    }
+    return accMul < accSum**2 ? 1 : 0;
+}
+*/
