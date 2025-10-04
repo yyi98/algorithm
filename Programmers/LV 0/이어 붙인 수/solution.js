95c3afb4a71ce8b385e4a0729ab5dbd943d73e62
yyi98 <dudrn170424@gmail.com>
2025-10-04 21:33:32 +0900

    ★ 중요

--- Programmers/LV 0/이어 붙인 수/solution.js
+++ Programmers/LV 0/이어 붙인 수/solution.js
@@ -1,9 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181928
 function solution(num_list) {
-    var answer = 0;
     let {odds, evens} = num_list.reduce(({odds, evens}, cur) => {
-        cur % 2 == 0 ? evens.push(cur) : odds.push(cur);
+        if (cur % 2 == 0)
+            evens.push(cur);
+        else
+            odds.push(cur);
         return {odds, evens}
-    }, {odds : [], evens: []})
-    return Number(odds.join('')) + Number(evens.join(''));
+    }, {odds: [], evens:[]});
+    return Number(odds.join("")) + Number(evens.join(""));
 }
