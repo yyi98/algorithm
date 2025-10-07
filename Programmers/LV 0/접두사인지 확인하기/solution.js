a4ac03cdbe49d429c4dc8c637608e16ca878a331
yyi98 <dudrn170424@gmail.com>
2025-10-07 20:30:04 +0900

    Create solution.js

---
+++ Programmers/LV 0/접두사인지 확인하기/solution.js
@@ -0,0 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181906
+function solution(my_string, is_prefix) {
+    return my_string.slice(0, is_prefix.length) == is_prefix ? 1 : 0;
+}
