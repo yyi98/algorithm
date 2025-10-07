bdd822d65ceb38c8e130a4b27cbaf4fa65c51835
yyi98 <dudrn170424@gmail.com>
2025-10-07 15:46:58 +0900

    Update solution.js

--- Programmers/LV 0/자릿수 더하기/solution.js
+++ Programmers/LV 0/자릿수 더하기/solution.js
@@ -1,4 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120906
+// function solution(n) {
+//     return [...n.toString()].map(x => Number(x)).reduce((acc, cur) => { return acc + cur });
+// }
+
+
 function solution(n) {
-    return [...n.toString()].map(x => Number(x)).reduce((acc, cur) => { return acc + cur });
+    return [...n.toString()].map(Number).reduce((acc, cur) => {return acc + cur});
 }
