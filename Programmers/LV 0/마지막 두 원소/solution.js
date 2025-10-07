0aa5cd3f3b09127aa0a790974b25e3f57482fb60
yyi98 <dudrn170424@gmail.com>
2025-10-08 01:34:52 +0900

    Update solution.js

--- Programmers/LV 0/마지막 두 원소/solution.js
+++ Programmers/LV 0/마지막 두 원소/solution.js
@@ -1,5 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181927
 function solution(num_list) {
-    let [a,b] = [...num_list].reverse();
-    return [...num_list, a > b ? a - b : a * 2];
+    let [a, b] = num_list.slice(-2);
+    b > a ? num_list.push(b - a) : num_list.push(b * 2);
+    return num_list;
 }
